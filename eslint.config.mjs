0eb3b8e0607f19a5c6bcbc73b51e05dc82c81338
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-02-21 22:06:35 +0100

    all3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Wyłączenie błędu dla nieużywanych zmiennych
+      '@typescript-eslint/no-unused-vars': 'off',
+
+      // Wyłączenie błędu dla nieescapowanych znaków w JSX
+      'react/no-unescaped-entities': 'off',
+
+      // Wyłączenie błędu dla zależności useEffect
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
